fe948a83f01d5a3526156b4a86b732f06e543aeb
Arnav Balyan <arnavb@uber.com>
2025-05-11 21:53:03 +0530

    update

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -416,9 +416,6 @@ class vLLMRollout(BaseRollout):
         Yields:
             tuple: (prompt_idx, DataProto) containing the original prompt index and its generated sequence
         """
-        import traceback
-        print("generate_sequences for fsdp_worker")
-        traceback.print_stack()
         assert self.config.async_engine, "generate_sequences_async requires async_engine=True"
         # rebuild vllm cache engine
         if vllm_version in ('0.3.1', '0.4.2', '0.5.4', '0.6.3') and self.config.free_cache_engine:
