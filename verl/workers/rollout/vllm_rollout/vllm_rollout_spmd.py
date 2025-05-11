9e29c492ea346bc0d2d35c9cb3f3bbcf2dfbefaa
Arnav Balyan <arnavb@uber.com>
2025-05-11 18:37:48 +0530

    udapte

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -254,6 +254,7 @@ class vLLMRollout(BaseRollout):
 
     @torch.no_grad()
     def generate_sequences(self, prompts: DataProto, **kwargs) -> DataProto:
+        print("generate_sequences for vllm_rollout_spmd")
         # Handle async engine case
         if self.config.async_engine:
             outputs = []
@@ -415,6 +416,7 @@ class vLLMRollout(BaseRollout):
         Yields:
             tuple: (prompt_idx, DataProto) containing the original prompt index and its generated sequence
         """
+        print("generate_sequences_async", batch)
         assert self.config.async_engine, "generate_sequences_async requires async_engine=True"
         # rebuild vllm cache engine
         if vllm_version in ('0.3.1', '0.4.2', '0.5.4', '0.6.3') and self.config.free_cache_engine:
@@ -435,11 +437,13 @@ class vLLMRollout(BaseRollout):
             raise RuntimeError('vllm sharding manager is not work properly.')
 
         if 'multi_modal_data' in non_tensor_batch:
+            print("processing multi-modal data")
             vllm_inputs = []
             for raw_prompt_ids, multi_modal_data in zip(non_tensor_batch.pop('raw_prompt_ids'),
                                                         non_tensor_batch.pop('multi_modal_data')):
                 vllm_inputs.append({'prompt_token_ids': raw_prompt_ids, 'multi_modal_data': multi_modal_data})
         else:
+            print("processing non-multi-modal data")
             vllm_inputs = [{
                 'prompt_token_ids': raw_prompt_ids
             } for raw_prompt_ids in non_tensor_batch.pop('raw_prompt_ids')]
