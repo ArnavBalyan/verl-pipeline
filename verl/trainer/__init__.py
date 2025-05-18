1ba6ea8b99218e03d4e6523c0f584165a8546898
Arnav Balyan <arnavb@uber.com>
2025-05-18 17:17:38 +0530

    update

--- verl/trainer/__init__.py
+++ verl/trainer/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from .safe_actor_rollout_ref import SafeActorRolloutRefWorker
+from .mem_permit import _MemPermit
+from .gpu_supervisor import GpuSupervisor
